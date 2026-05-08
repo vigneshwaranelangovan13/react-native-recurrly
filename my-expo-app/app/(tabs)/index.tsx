857d26610ba921fca3425349d080cc90a4b5a361
vigneshwaranelangovan13 <vigneshwaranelangovan13@gmail.com>
2026-05-07 21:49:16 -0500

    implement fonts

--- my-expo-app/app/(tabs)/index.tsx
+++ my-expo-app/app/(tabs)/index.tsx
@@ -4,8 +4,8 @@ import { Text, View } from "react-native";
 export default function Index() {
     return (
         <View className="flex-1 items-center justify-center bg-background">
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
+            <Text className="text-7xl font-sans-extrabold ">
+                Home
             </Text>
         </View>
     );
